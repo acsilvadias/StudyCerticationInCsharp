cec7fb6a2827e5b1447406e7e8c9db551df37e08
Antonio Carlos <avanade.antonio@voeazul.com.br>
2020-04-01 12:53:17 -0300

    Adicionado Listing 1 a 5 no chapter1

--- Chapter 1/Listen_1_1/ParallelInvokeConsole/ParallelListing/ParallelListing/Program.cs
+++ Chapter 1/Listen_1_1/ParallelInvokeConsole/ParallelListing/ParallelListing/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Linq;
 
 namespace ParallelListing
 {
@@ -9,14 +10,106 @@ namespace ParallelListing
 
         static void Main(string[] args)
         {
-            CallParallelInvoke();
+            //CallParallelInvoke();
+            //CallParallelForEach();
+            //CallParallelForEachFor();
+            //CallParallelLoopResult();
+            CallParallelLinqQuery();
 
             Console.WriteLine("Finished processing. Press a key to end.");
             Console.ReadKey();
         }
 
 
-    #region ***** Parallel.Invoke *****
+        #region ***** Parallel.Linq.Query Listing 5 *****
+        class Person
+        {
+            public string Name { get; set; }
+            public string City { get; set; }
+
+        }
+        static void CallParallelLinqQuery()
+        {
+            Person[] people = new Person[]
+            {
+                new Person { Name = "Alan", City = "Hull" },
+                new Person { Name = "Beryl", City = "Seattle" },
+                new Person { Name = "Charles", City = "London" },
+                new Person { Name = "David", City = "Seattle" },
+                new Person { Name = "Eddy", City = "Paris" },
+                new Person { Name = "Fred", City = "Berlin" },
+                new Person { Name = "Gordon", City = "Hull" },
+                new Person { Name = "Henry", City = "Seattle" },
+                new Person { Name = "Isaac", City = "Seattle" },
+                new Person { Name = "James", City = "London" }
+            };
+
+            var result = from person in people.AsParallel()
+                         where person.City == "Seattle"
+                         select person;
+            foreach (var person in result)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+
+        }
+
+        #endregion
+
+        #region ***** ParallelLoopResult Listing 4 *****
+        static void CallParallelLoopResult()
+        {
+            var items = Enumerable.Range(0, 500).ToArray();
+
+            ParallelLoopResult result = Parallel.For(0,items.Count(),(int i, ParallelLoopState p) =>{
+                if (i == 200)
+                    p.Stop();
+
+                WorkOnItem(items[i]);
+
+            });
+
+            Console.WriteLine("Completed: " + result.IsCompleted);
+            Console.WriteLine("Items: " + result.LowestBreakIteration);
+        }
+        #endregion
+
+        #region ***** Parallel.For Listing 3 *****
+
+        private static void CallParallelForEachFor()
+        {
+            var items = Enumerable.Range(0, 500).ToArray();
+
+            Parallel.For(0, items.Length, i =>
+            {
+                WorkOnItem(items[i]);
+
+            });
+        }
+
+        #endregion
+
+        #region ***** Parallel.ForEach Listing 2 *****
+        private static void CallParallelForEach()
+        {
+            var items = Enumerable.Range(0, 500);
+            Parallel.ForEach(items, item =>
+            {
+                WorkOnItem(item);
+            });
+        }
+
+
+        #endregion
+
+        #region ***** Parallel.Invoke Listing 1 *****
+        static void WorkOnItem(object item)
+        {
+            Console.WriteLine("Starting Working om: " + item);
+            Thread.Sleep(100);
+            Console.WriteLine("Finished Working om: " + item);
+        }
         private static void CallParallelInvoke()
         {
             Parallel.Invoke(() => Task1(), () => Task2());
@@ -34,6 +127,6 @@ namespace ParallelListing
             Thread.Sleep(2000);
             Console.WriteLine("Task1 2 ending");
         }
-    #endregion
+        #endregion
     }
 }
